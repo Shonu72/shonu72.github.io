1c2f7d79a59373c56a9db38b53a0f25e4682db5f
Shonu72 <shourya7250@gmail.com>
2022-06-30 21:30:01 +0530

    updated portfolio

--- mail/contact.php
+++ mail/contact.php
@@ -9,7 +9,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "shonu7250@pm.me"; // Change this email to your //
+$to = "sonu.shour77@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
